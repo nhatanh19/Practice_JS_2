7d71adec1122088e29d7e756b8d9c5e58e25c321
Nhật Ánh <nhatanh19a10@gmail.com>
2025-01-17 13:41:43 +0700

    create interface page signup #3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,14 @@ import {
     NotFoundPage
 } from '../views/pages/not-found.js';
 
+import {
+    LoginPage
+} from '../views/pages/login.js';
+
+import {
+    RegisterPage
+} from '../views/pages/signup.js';
+
 export const routes = {
     '/': {
         component: HomePage,
@@ -21,5 +29,13 @@ export const routes = {
     '/404': {
         component: NotFoundPage,
         title: 'Page Not Found'
+    },
+    '/login': {
+        component: LoginPage,
+        title: 'Login'
+    },
+    '/signup': {
+        component: RegisterPage,
+        title: 'Sign Up'
     }
 };
