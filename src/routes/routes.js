3ca49a1a524cf387e8b273a1fbc6202ccfafc484
Nhật Ánh <nhatanh19a10@gmail.com>
2025-02-06 00:51:07 +0700

    Complete  page success verry email your hotel

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,6 +9,19 @@ import {
     NotFoundPage
 } from '../views/pages/not-found.js';
 
+<<<<<<< HEAD
+=======
+import {
+    LoginPage
+} from '../views/pages/login.js';
+
+import {
+    RegisterPage
+} from '../views/pages/signup.js';
+
+import SuccessPage from "../views/pages/success";
+
+>>>>>>> 4968959 (Complete  page success verry email your hotel)
 export const routes = {
     '/': {
         component: HomePage,
@@ -21,5 +34,20 @@ export const routes = {
     '/404': {
         component: NotFoundPage,
         title: 'Page Not Found'
+<<<<<<< HEAD
+=======
+    },
+    '/login': {
+        component: LoginPage,
+        title: 'Login'
+    },
+    '/signup': {
+        component: RegisterPage,
+        title: 'Sign Up'
+    },
+    '/success-verify-email': {
+        component: SuccessPage,
+        title: 'Success verry email your hotel'
+>>>>>>> 4968959 (Complete  page success verry email your hotel)
     }
 };
